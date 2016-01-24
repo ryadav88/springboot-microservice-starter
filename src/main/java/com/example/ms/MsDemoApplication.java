6996f09b8c64c94a38f49d154b16eaeefeeefb3b
Ravi Yadav <ravi.yadav@weather.com>
2016-01-24 15:03:18 -0800

    added remote shell, web, connection to postgres

--- src/main/java/com/example/ms/MsDemoApplication.java
+++ src/main/java/com/example/ms/MsDemoApplication.java
@@ -3,8 +3,12 @@ package com.example.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan
 @EnableAutoConfiguration
 public class MsDemoApplication {
 
